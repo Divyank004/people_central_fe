e96899ecb707e0abf9343b9869449605dd3f1337
Divyank Dhadi <divyank004@gmail.com>
2025-06-18 14:25:29 +0200

    add toolbar buttons routing

---
+++ src/pages/TaskPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row q-ma-md">
+   <div>TaskPage Page</div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,11 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/absences', component: () => import('pages/AbsencesPage.vue') },
+      { path: '/team', component: () => import('pages/TeamPage.vue') },
+      { path: '/tasks', component: () => import('pages/TaskPage.vue') }
+    ],
   },
 
   // Always leave this as last one,
